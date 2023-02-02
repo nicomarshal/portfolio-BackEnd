2617b43d2f530089350562499aff51ca9b15bc0b
Nico Ledesma <dev.ledesmanicolas@gmail.com>
2023-02-02 10:24:39 -0300

    Agregando entidades Education y EducationType

---
+++ src/main/java/com/nicomarshal/portfolio/model/EducationType.java
@@ -0,0 +1,13 @@
+package com.nicomarshal.portfolio.model;
+
+import lombok.Getter;
+
+@Getter
+public class EducationType {
+    private Long id;
+    private String type;
+    private String description;
+
+    public EducationType() {
+    }
+}
