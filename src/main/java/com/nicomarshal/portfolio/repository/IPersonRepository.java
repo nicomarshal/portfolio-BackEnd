fec99ccb6dc9f1c7f9295a606aea42f6ff976534
Nico Ledesma <dev.ledesmanicolas@gmail.com>
2023-02-02 11:34:02 -0300

    Agregando repositorio Person (2)

--- src/main/java/com/nicomarshal/portfolio/repository/IPersonRepository.java
+++ src/main/java/com/nicomarshal/portfolio/repository/IPersonRepository.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.nicomarshal.portfolio.repository;
 
-/**
- *
- * @author admin
- */
-public interface IPersonRepository {
-    
+import com.nicomarshal.portfolio.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/*La interface extiende de JpaRepository (que maneja reposi
+torios JPA)
+En los parámetros <>  deben ir: <clase a persistir, tipo de dato de su id>*/
+@Repository
+public interface IPersonRepository extends JpaRepository<Person, Long> {    
 }
