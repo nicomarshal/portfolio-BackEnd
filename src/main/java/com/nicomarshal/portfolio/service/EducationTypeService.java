f8cf0290c2f291dab9840f8b6507655f012f05f6
Nico Ledesma <dev.ledesmanicolas@gmail.com>
2023-02-02 19:55:29 -0300

    Agregando servicios EducationType

---
+++ src/main/java/com/nicomarshal/portfolio/service/EducationTypeService.java
@@ -0,0 +1,33 @@
+package com.nicomarshal.portfolio.service;
+
+import com.nicomarshal.portfolio.model.EducationType;
+import com.nicomarshal.portfolio.repository.IEducationTypeRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EducationTypeService implements IEducationTypeService{
+    @Autowired
+    private IEducationTypeRepository educationTypeRepository;
+
+    @Override
+    public List<EducationType> getEducationTypes() {
+        List<EducationType> listEducationTypes = educationTypeRepository.findAll();
+        return listEducationTypes;
+    }
+
+    @Override
+    public void saveEducationType(EducationType educationType) {
+        educationTypeRepository.save(educationType);
+    }
+
+    @Override
+    public void deleteEducationType(Long id) {
+        educationTypeRepository.deleteById(id);
+    }
+
+    @Override
+    public EducationType findEducationType(Long id) {
+        EducationType educationType = educationTypeRepository.findById(id).orElse(null);
+        return educationType;
+    }    
+}
